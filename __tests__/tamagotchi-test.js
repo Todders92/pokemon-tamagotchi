03c12fe07ccf9c0996fa34882c148b9c3ed06f99
Jamison Cozart and Michelle Morin <jamison@>
2020-02-10 09:39:01 -0800

    add more expect statements to first test, fail

--- __tests__/tamagotchi-test.js
+++ __tests__/tamagotchi-test.js
@@ -1,11 +1,16 @@
-import { Tamagotchi } from '../src/tamagotchi.js'
+import { Tamagotchi } from '../src/tamagotchi.js';
 
 describe('Tamagotchi', () => {
   let tamagotchi;
   beforeEach(() => {
     tamagotchi = new Tamagotchi();
   });
-  test('should correctly create a Tamagotchi object', () => {
+  test('should correctly create a Tamagotchi object with zero food, happiness, and sleep levels', () => {
     expect(tamagotchi.name).toEqual("");
+    expect(tamagotchi.sleep).toBe(0);
+    expect(tamagotchi.happiness).toBe(0);
+    expect(tamagotchi.foodLevel).toBe(0);
   });
+
+  // test('should instantiate a Tama')
 });
