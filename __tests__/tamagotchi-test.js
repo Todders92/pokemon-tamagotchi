c036a61c4db3df9b9555238e6d30390dba016b0e
Jamison Cozart and Michelle Morin <michelle@>
2020-02-10 15:11:40 -0800

    update timing again

--- __tests__/tamagotchi-test.js
+++ __tests__/tamagotchi-test.js
@@ -16,98 +16,98 @@ describe('Tamagotchi', () => {
     expect(tamagotchi.foodLevel).toBe(10);
   });
 
-  test('should check that food level is decremented every 10 seconds', () => {
+  test('should check that food level is decremented every second', () => {
     tamagotchi.getHungry();
-    jest.advanceTimersByTime(1000 * 10 + 1);
+    jest.advanceTimersByTime(1000 + 1);
     expect(tamagotchi.foodLevel).toBe(9);
   });
 
-  test('should decrement sleep level after every 10 seconds', () => {
+  test('should decrement sleep level after every second', () => {
     tamagotchi.getTired();
-    jest.advanceTimersByTime(1000 * 10 + 1);
+    jest.advanceTimersByTime(1000 + 1);
     expect(tamagotchi.sleep).toBe(9);
   });
 
-  test('should check that happiness is decremented every 10 seconds', () => {
+  test('should check that happiness is decremented every second', () => {
     tamagotchi.getSad();
-    jest.advanceTimersByTime(1000 * 10 + 1);
+    jest.advanceTimersByTime(1000*2 + 1);
     expect(tamagotchi.happiness).toBe(8);
   });
 
   test('should refill tamagotchi food level', () => {
     tamagotchi.getHungry();
-    jest.advanceTimersByTime(1000 * 10 + 1);
+    jest.advanceTimersByTime(1000 + 1);
     tamagotchi.feed();
     expect(tamagotchi.foodLevel).toBe(10);
   });
 
-  test('should stay busy for 15 seconds after feeding', () => {
+  test('should stay busy for 2 seconds after feeding', () => {
     tamagotchi.feed();
     expect(tamagotchi.busy).toBe(true);
-    jest.advanceTimersByTime(1000*15 +1);
+    jest.advanceTimersByTime(1000*2 +1);
     expect(tamagotchi.busy).toBe(false);
   });
 
   test('should refill tamagotchi sleep level', () => {
     tamagotchi.getTired();
-    jest.advanceTimersByTime(1000 * 10 + 1);
+    jest.advanceTimersByTime(1000 + 1);
     tamagotchi.bedTime();
     expect(tamagotchi.sleep).toBe(10);
   });
 
-  test('should stay asleep for 15 seconds', () => {
+  test('should stay asleep for 2 seconds', () => {
     tamagotchi.bedTime();
     expect(tamagotchi.busy).toBe(true);
-    jest.advanceTimersByTime(1000*15 +1);
+    jest.advanceTimersByTime(1000*2 +1);
     expect(tamagotchi.busy).toBe(false);
   });
 
   test('should check that play() refills happiness', () => {
     tamagotchi.getSad();
-    jest.advanceTimersByTime(1000 * 10 + 1);
+    jest.advanceTimersByTime(1000 + 1);
     tamagotchi.play();
     expect(tamagotchi.happiness).toBe(10);
   });
 
-  test('should check whether tamagotchi is busy for 15 seconds after playing', () => {
+  test('should check whether tamagotchi is busy for 2 seconds after playing', () => {
     tamagotchi.play();
     expect(tamagotchi.busy).toBe(true);
-    jest.advanceTimersByTime(1000*15 +1);
+    jest.advanceTimersByTime(1000*2 +1);
     expect(tamagotchi.busy).toBe(false);
   });
 
   test('should prevent feed methods from triggering if busy', () => {
     tamagotchi.getHungry();
-    jest.advanceTimersByTime(1000*10 + 1);
+    jest.advanceTimersByTime(1000 + 1);
     tamagotchi.bedTime();
     tamagotchi.feed();
     expect(tamagotchi.busy).toBe(true);
     expect(tamagotchi.foodLevel).toBe(9);
-    jest.advanceTimersByTime(1000*15 +1);
+    jest.advanceTimersByTime(1000*2 +1);
     tamagotchi.feed();
     expect(tamagotchi.foodLevel).toBe(10);
   });
 
   test('should prevent bedtime method from triggering if busy', () => {
     tamagotchi.getTired();
-    jest.advanceTimersByTime(1000*10 +1);
+    jest.advanceTimersByTime(1000 +1);
     tamagotchi.feed();
     tamagotchi.bedTime();
     expect(tamagotchi.busy).toBe(true);
     expect(tamagotchi.sleep).toBe(9);
-    jest.advanceTimersByTime(1000*15 +1);
+    jest.advanceTimersByTime(1000*2 +1);
     tamagotchi.bedTime();
     expect(tamagotchi.sleep).toBe(10);
   });
 
   test('should prevent play method from triggering if busy', () => {
     tamagotchi.getSad();
-    jest.advanceTimersByTime(1000*10+1);
+    jest.advanceTimersByTime(1000*2 +1);
     tamagotchi.bedTime();
     tamagotchi.play();
     expect(tamagotchi.busy).toBe(true);
     expect(tamagotchi.happiness).toBe(8);
-    jest.advanceTimersByTime(1000*15 +1);
+    jest.advanceTimersByTime(1000*2 +1);
     tamagotchi.play();
     expect(tamagotchi.happiness).toBe(10);
   });
@@ -115,18 +115,18 @@ describe('Tamagotchi', () => {
   test('should kill tamagotchi when two of three status bars are 0', () => {
     tamagotchi.getHungry();
     tamagotchi.getSad();
-    jest.advanceTimersByTime(1000*10*10 +1);
+    jest.advanceTimersByTime(1000*10 +1);
     tamagotchi.checkAlive();
-    jest.advanceTimersByTime(1000 +1);
+    jest.advanceTimersByTime(500 +1);
     expect(tamagotchi.alive).toBe(false);
   });
 
   test('should check that user cannot feed, play with, or rest tamagotchi after it dies', () => {
     tamagotchi.getHungry();
     tamagotchi.getSad();
-    jest.advanceTimersByTime(1000*10*10 +1);
+    jest.advanceTimersByTime(1000*10 +1);
     tamagotchi.checkAlive();
-    jest.advanceTimersByTime(1000 +1);
+    jest.advanceTimersByTime(500 +1);
     tamagotchi.feed();
     expect(tamagotchi.foodLevel).toBe(0);
   });
--- src/main.js
+++ src/main.js
@@ -6,9 +6,39 @@ import './styles.css';
 
 function updateStatus(Tamagotchi) {
   setInterval(function() {
+    if(Tamagotchi.foodLevel < 3) {
+      $("#foodDiv .progress-bar").attr('class', 'progress-bar progress-bar-striped progress-bar-animated bg-danger');
+    } else if(Tamagotchi.foodLevel <= 6) {
+      $("#foodDiv .progress-bar").attr('class', 'progress-bar progress-bar-striped progress-bar-animated bg-warning');
+    } else if(Tamagotchi.foodLevel > 6) {
+      $("#foodDiv .progress-bar").attr('class', 'progress-bar progress-bar-striped progress-bar-animated bg-success');
+    }
+    if(Tamagotchi.sleep < 3) {
+      $("#sleepDiv .progress-bar").attr('class', 'progress-bar progress-bar-striped progress-bar-animated bg-danger');
+    } else if(Tamagotchi.sleep <= 6) {
+      $("#sleepDiv .progress-bar").attr('class', 'progress-bar progress-bar-striped progress-bar-animated bg-warning');
+    } else if(Tamagotchi.sleep > 6) {
+      $("#sleepDiv .progress-bar").attr('class', 'progress-bar progress-bar-striped progress-bar-animated bg-success');
+    }
+    if(Tamagotchi.happiness < 3) {
+      $("#sadDiv .progress-bar").attr('class', 'progress-bar progress-bar-striped progress-bar-animated bg-danger');
+    } else if(Tamagotchi.happiness <= 6) {
+      $("#sadDiv .progress-bar").attr('class', 'progress-bar progress-bar-striped progress-bar-animated bg-warning');
+    } else if(Tamagotchi.happiness > 6) {
+      $("#sadDiv .progress-bar").attr('class', 'progress-bar progress-bar-striped progress-bar-animated bg-success');
+    }
     $("#foodDiv .progress-bar").attr('style', `width: ${Tamagotchi.foodLevel*10}%`);
     $("#sleepDiv .progress-bar").attr('style', `width: ${Tamagotchi.sleep*10}%`);
     $("#sadDiv .progress-bar").attr('style', `width: ${Tamagotchi.happiness*10}%`);
+  }, 1000);
+}
+
+function endGame(Tamagotchi) {
+  setInterval(function() {
+    if (Tamagotchi.alive === false) {
+      $("#death").show();
+      $("#game").hide();
+    }
   }, 1000);
 }
 
@@ -42,6 +72,7 @@ $(document).ready(function() {
     $("#game").show();
     $("#choiceImg").attr('src', img);
     updateStatus(tamagotchi);
+    endGame(tamagotchi);
     tamagotchi.getHungry();
     tamagotchi.getSad();
     tamagotchi.getTired();
@@ -59,11 +90,8 @@ $(document).ready(function() {
       tamagotchi.play();
     });
 
-    if (tamagotchi.alive === false) {
-      $("button#reset").show();
-      $("button#reset").click(function() {
-        tamagotchi.reset();
-      });
-    }
+  });
+  $("button#reset").click(function() {
+    tamagotchi.reset();
   });
 });
